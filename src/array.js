38bb0fd1c2d59914b16f82819878b12aadeea3ca
Sadi Kaya <skay@oticon.com>
2017-12-14 09:37:16 +0100

    1.4.1

--- src/array.js
+++ src/array.js
@@ -1,19 +1,11 @@
 /**
- * We first assign the local variable `arrayFn` to the implicit `exports` object and then
- * assigns function references to the properties of `arrayFn`. Because `arrayFn` references
- * `exports` and `exports` is the object you get when you call `require("arrayFn")` anything
- * assigned to `arrayFn` will be available on the object you get from `require`.
- */
-const arrayFn = module.exports;
-
-/**
  * Array difference (complement)
  *   Create a `Set` from `b`, then use `Array.filter()` on `a` to only keep values not contained in `b`.
  * @param a
  * @param b
  * @returns {Int32Array | * | Uint32Array | T[] | Int8Array | Float64Array}
  */
-arrayFn.difference = (a, b) => {
+module.exports.difference = (a, b) => {
   const s = new Set(b);
   return a.filter(x => !s.has(x)); // difference([1,2,3], [1,2]) -> [3]
 };
@@ -25,7 +17,7 @@ arrayFn.difference = (a, b) => {
  * @param b
  * @returns {Int32Array | * | Uint32Array | T[] | Int8Array | Float64Array}
  */
-arrayFn.intersection = (a, b) => {
+module.exports.intersection = (a, b) => {
   const s = new Set(b);
   return a.filter(x => s.has(x)); // intersection([1,2,3], [1,2]) -> [2,3]
 };
@@ -37,7 +29,7 @@ arrayFn.intersection = (a, b) => {
  * @param b
  * @returns {any[]}
  */
-arrayFn.union = (a, b) => {
+module.exports.union = (a, b) => {
   return Array.from(new Set([...a, ...b])); // union([1,2,3], [4,3,2]) -> [1,2,3,4]
 };
 
@@ -48,7 +40,7 @@ arrayFn.union = (a, b) => {
  * @param values
  * @returns {Int32Array | * | Uint32Array | T[] | Int8Array | Float64Array}
  */
-arrayFn.similarity = (arr, values) => {
+module.exports.similarity = (arr, values) => {
   return arr.filter(v => values.includes(v)); // similarity([1,2,3], [1,2,4] -> [1,2]
 };
 
@@ -61,7 +53,7 @@ arrayFn.similarity = (arr, values) => {
  * @param size
  * @returns {(any)[]}
  */
-arrayFn.chunk = (arr, size) => {
+module.exports.chunk = (arr, size) => {
   return Array
     .apply(null, { length: Math.ceil(arr.length / size) })
     .map((v, i) => arr.slice(i * size, i * size + size)); // chunk([1,2,3,4,5], 2) -> [[1,2], [3,4], 5]
@@ -73,7 +65,7 @@ arrayFn.chunk = (arr, size) => {
  * @param arr
  * @param value
  */
-arrayFn.countOccurrences = (arr, value) => {
+module.exports.countOccurrences = (arr, value) => {
   return arr.reduce((a, v) => v === value ? a + 1 : a + 0, 0); // countOccurrences([1,1,2,1,2,3], 1) -> 3
 };
 
@@ -82,7 +74,7 @@ arrayFn.countOccurrences = (arr, value) => {
  *   Use `Array.reduce()` to get all elements inside the array and `concat()` to flatten them.
  * @param arr
  */
-arrayFn.flatten = (arr) => {
+module.exports.flatten = (arr) => {
   return arr.reduce((a, v) => a.concat(v), []); // flatten([1,[2],3,4]) -> [1,2,3,4]
 };
 
@@ -92,7 +84,7 @@ arrayFn.flatten = (arr) => {
  * @param arr
  * @returns {*[]}
  */
-arrayFn.unique = (arr) => {
+module.exports.unique = (arr) => {
   return [...new Set(arr)]; // unique([1,2,2,3,4,4,5]) -> [1,2,3,4,5]
 };
 
@@ -102,6 +94,6 @@ arrayFn.unique = (arr) => {
  * @param arr
  * @returns {*}
  */
-arrayFn.shuffle = (arr) => {
+module.exports.shuffle = (arr) => {
   return arr.sort(() => Math.random() >= 0.5 ? -1 : 1); // shuffle([1,2,3]) -> [1,3,2]
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-module.exports.arrayFn = require('./array');
-module.exports.asset = require('./asset');
-module.exports.log = require('./log');
-module.exports.parse = require('./parse');
-module.exports.preloadImages = require('./preloadImages');
-module.exports.URL = require('./URL');
-module.exports.UUID = require('./UUID');
+module.exports = {
+  arrayFn: {
+    difference: require('./array').difference,
+    intersection: require('./array').intersection,
+    union: require('./array').union,
+    similarity: require('./array').similarity,
+    chunk: require('./array').chunk,
+    countOccurrences: require('./array').countOccurrences,
+    flatten: require('./array').flatten,
+    unique: require('./array').unique,
+    shuffle: require('./array').shuffle,
+  },
+  asset: require('./asset'),
+  log: require('./log'),
+  parse: require('./parse'),
+  url: {
+    parameters: require('./URL').parameters,
+    redirect: require('./URL').redirect,
+  },
+  uuid: require('./UUID'),
+};
