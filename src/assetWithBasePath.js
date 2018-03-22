2fd03bd506bcad1bbc0cf6076e187a0a05821569
Sadi Kaya <skay@oticon.com>
2018-03-22 12:05:55 +0100

    added dev flag to assetWithBasePath

--- src/assetWithBasePath.js
+++ src/assetWithBasePath.js
@@ -4,8 +4,8 @@
  * @param basePath
  * @returns {*}
  */
-function assetWithBasePath(asset, basePath) {
-  if (process.env.NODE_ENV === "production") {
+function assetWithBasePath(asset, basePath, dev) {
+  if (process.env.NODE_ENV === "production" && !dev) {
     if (asset.mediapath === undefined || asset.mediaurl === undefined) {
       return undefined;
     }
